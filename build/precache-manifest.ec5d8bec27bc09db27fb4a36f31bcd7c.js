de9ec3c4a533e3d7c5c179e3c8a56eeb1216882e
WilkRhu <wilk.caetano@gmail.com>
2020-03-02 10:32:15 -0300

    build

---
+++ build/precache-manifest.ec5d8bec27bc09db27fb4a36f31bcd7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "701729b8cea5d82843dfd7ad97c925a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f3bfd797e1f6574dee5",
+    "url": "/static/css/main.830c1ef7.chunk.css"
+  },
+  {
+    "revision": "1f1e8543c5df7084f0de",
+    "url": "/static/js/2.264d0ebe.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.264d0ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f3bfd797e1f6574dee5",
+    "url": "/static/js/main.3ad551d4.chunk.js"
+  },
+  {
+    "revision": "1dc190bf92ae26fe5c0c",
+    "url": "/static/js/runtime-main.ec38d303.js"
+  }
+]);
