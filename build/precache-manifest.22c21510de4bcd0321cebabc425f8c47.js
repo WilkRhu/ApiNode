ca562d325a092e0dd7fb05c8d4511e0e4316a001
WilkRhu <wilk.caetano@gmail.com>
2020-03-02 11:02:57 -0300

    build modified routes new

---
+++ build/precache-manifest.22c21510de4bcd0321cebabc425f8c47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b949e7fe13b96769050dbd28f8fa54d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04a9b3ce8dd08fbee516",
+    "url": "/static/css/main.830c1ef7.chunk.css"
+  },
+  {
+    "revision": "1f1e8543c5df7084f0de",
+    "url": "/static/js/2.264d0ebe.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.264d0ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a9b3ce8dd08fbee516",
+    "url": "/static/js/main.a6e4c87b.chunk.js"
+  },
+  {
+    "revision": "1dc190bf92ae26fe5c0c",
+    "url": "/static/js/runtime-main.ec38d303.js"
+  }
+]);
