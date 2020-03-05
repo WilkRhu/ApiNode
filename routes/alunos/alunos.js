e7742bccfdc5ebbe5bf6cfdb3d5f718ca482037f
WilkRhu <wilk.caetano@gmail.com>
2020-03-05 10:57:15 -0300

    Creation Login router

--- routes/alunos/alunos.js
+++ routes/alunos/alunos.js
@@ -30,6 +30,25 @@ router.post('/create', async (req, res) => {
     }catch(err) {
         return res.status(500).send({err: 'Erro ao criar Aluno!'});
     }
-})
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) return res.status(400).send({error: 'Dados insuficientes!'});
+    try{
+        const alunos = await Alunos.findOne({email}).select('+password');
+        
+         if(!alunos) return res.status(400).send({error: 'Usuário e/ou senha inválidos!'});
+        
+        const pass_ok = await bcrypt.compare(password, alunos.password);
+        if(!pass_ok) return res.status(401).send({error: 'Usuário e/ou senha inválidos'});
+
+        alunos.password = undefined;
+        return res.send({alunos, token: createUserToken(alunos.id)});
+    }
+    catch(err){
+        return res.status(500).send({error:'Não autorizado'});
+    }
+});
 
 module.exports = router;
